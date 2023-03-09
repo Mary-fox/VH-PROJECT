b83d894f2f500527a2233045e2086d9d7dc2717a
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-03-09 12:41:46 +0300

    feat: add burger menu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const sliderContainer = document.querySelector(".slider__content");
+//slider
+
 const prevBtn = document.querySelector(".slider__arrow_prew");
 const nextBtn = document.querySelector(".slider__arrow_next");
 const slides = document.querySelectorAll(".slider__item");
@@ -25,3 +26,46 @@ function nextSlide() {
 prevBtn.addEventListener("click", prevSlide);
 nextBtn.addEventListener("click", nextSlide);
 
+//burger
+const body = document.body;
+const blackLayer = document.querySelector('.black-layer')
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const closePopup = document.querySelector(".popup__close")
+
+// Дубликат nav, для создания новых стилей
+const menu = document.querySelector(".nav__list").cloneNode(1);
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  blackLayer.classList.toggle('black-layer_active');
+  renderBurger();
+}
+
+//Рендер элементов
+function renderBurger() {
+  popup.appendChild(menu);
+}
+
+function closeRemove() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+  blackLayer.classList.remove('black-layer_active');
+}
+//Закрытие при клике на затемненную область
+blackLayer.addEventListener('click', () => {
+  closeRemove();
+})
+//Закрытие при клике на меню
+menu.addEventListener('click', () => {
+  closeRemove();
+})
+closePopup.addEventListener('click', () => {
+  closeRemove();
+})
